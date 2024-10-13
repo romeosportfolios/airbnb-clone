13814bac23804b81dc49f233a41d07c910613b01
Ali Raza <alirazaasif2511@gmail.com>
2024-10-14 00:39:19 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
+import Footer from "./Footer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchPage from "./SearchPage";
+
+function App() {
+  return (
+    //BEM
+    <div className="app">
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+
+        <Footer />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
